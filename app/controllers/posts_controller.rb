97d80f93f2cb199a4b9df0135786d572df46853b
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-07 21:23:57 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
 
   def index  # indexアクションを定義した
-    @posts = Post.all # 全てのレコードを@postに代入
+    @posts = Post.all.order(id: "DESC") # 全てのレコードを@postに代入,降順
   end
 
-  def new
-  end
+  # def new削除
+  # end削除
 
   def create
     Post.create(content: params[:content])
+    redirect_to acton: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "posts", to: "posts#index"
-  get "posts/new", to: "posts#new"
+  root to: 'posts#index'
+  # get "posts", to: "posts#index"より簡単にトップページへアクセスするためにルートパスに変更
+  # get "posts/new", to: "posts#new"投稿完了しましたは使わないため削除
   post "posts", to: "posts#create"
 end
