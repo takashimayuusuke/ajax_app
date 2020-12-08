5e881b95aeb1c3759b2f4043b39f27b651c50f5b
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-08 11:20:50 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # get "posts", to: "posts#index"より簡単にトップページへアクセスするためにルートパスに変更
   # get "posts/new", to: "posts#new"投稿完了しましたは使わないため削除
   post "posts", to: "posts#create"
+  # メモのidを取得できるようにするためのルーティング設定(queryパラメーター)
+  # get 'posts', to: 'posts#checked'
+  # メモのidを取得できるようにするためのルーティング設定(pathパラメーター)
+  get 'posts/:id', to: 'posts#checked'
 end
